6c7e75c4456a23cbf06d518d74fb8aef41bc4c51
Anushil Kumar <markanushil@gmail.com>
2016-08-16 18:25:54 +0530

    Simple addition

---
+++ STRHH.py
@@ -0,0 +1,3 @@
+# 12156 STRHH - Half of the half
+
+number = input("Enter the number of integers: ")
---
+++ TESTINT.py
@@ -0,0 +1,6 @@
+# TESTINT - Test 1 
+
+a = int(input(">>"))
+b = int(input(">>"))
+
+print(a + b)
