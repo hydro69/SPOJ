1f4bab45cdffac0634da3322a01bab5e86658e37
Anushil Kumar <markanushil@gmail.com>
2016-08-16 20:13:44 +0530

    take string in step of 2

--- STRHH.py
+++ STRHH.py
@@ -1,3 +1,12 @@
 # 12156 STRHH - Half of the half
 
-number = input("Enter the number of integers: ")
+
+number = int(input("Enter the number of test cases: "))
+line=[]                             # making a list to contains all the strings
+for i in range(number):
+    line.append(input("Enter the strings of 2*k length: "))
+
+# print(line) # print the list
+
+for stg in line:
+    print(stg[0:int(len(stg)/2):2])        
